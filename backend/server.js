01a380f90ccc0d0d3db94ce4ebc356022eb08ac4
Palvi Dhawan <palvidhawan@Palvis-MacBook-Air.local>
2024-09-29 11:41:12 +0530

    Added frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const notes = require("./data/notes");
+const dotenv = require("dotenv");
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  return res.send("Api is running");
+});
+
+app.get("/api/notes", (req, res) => {
+  return res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((note) => note._id === req.params.id);
+  return res.json(note);
+});
+
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
