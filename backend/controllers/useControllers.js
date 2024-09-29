b227322919f6b1244ae028438e9ff8a13f09d191
Palvi Dhawan <palvidhawan@Palvis-MacBook-Air.local>
2024-09-29 23:05:49 +0530

    generate JWT token on for login and register routes

--- backend/controllers/useControllers.js
+++ backend/controllers/useControllers.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const generateToken = require("../utils/generateToken");
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password, pic } = req.body;
@@ -25,6 +26,7 @@ const registerUser = asyncHandler(async (req, res) => {
       email: user.email,
       pic: user.pic,
       isAdmin: user.isAdmin,
+      token: generateToken(user._id)
     });
   } else {
     res.status(400);
@@ -38,10 +40,11 @@ const authUser = asyncHandler(async (req, res) => {
 const user = await User.findOne({email});
     if(user && (await user.matchPassword(password))){
         res.json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        pic: user.pic,
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          pic: user.pic,
+          token: generateToken(user._id),
         });
     }else{
         res.status(400);
